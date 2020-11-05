1b63891df69e15ff216ce3cce589bc0f43d1b519
Madeline <madeline.harrop@gmail.com>
2020-11-05 17:03:56 +0000

    weatherappfilesforneil

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const hbs = require("express-handlebars");
 const path = require("path");
 const getPlaceHolder = require("./lib/getplaceholder");
-
+const bodyParser = require("body-parser")
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+
 app.use(express.static(path.join(__dirname, "public")));
-app.set('views', path.join(__dirname, 'views')); 
-app.set("view engine", ".hbs");
 
 app.engine("hbs", hbs({
     defaultLayout:"main",
@@ -16,17 +18,70 @@ app.engine("hbs", hbs({
     partialsDir: path.join(__dirname, "views", "partials")
 }));
 
+
+app.set('views', path.join(__dirname, 'views')); 
+app.set("view engine", ".hbs");
+
+
 app.get("/", async (req, res) => {
-    let data = await getPlaceHolder();
-    res.render("index", {data, listExists: true});
+    let data = await getPlaceHolder("Manchester", "gb");
+    console.log(data);
+    let name = data.name;
+    let description = data.weather[0].description;
+    let temp = data.main.temp;
+    let feels_like = data.main.feel_like;
+    let sunrise = data.sys.sunrise;
+    let sunset = data.sys.sunset;
+    res.render("index", {
+        name, 
+        data:{description ,temp, feels_like, sunrise, sunset
+        }});
 });
-app.get("/about", (req, res) => {
-    res.render("about")
-})
+
+app.post("/weather", async(req, res) => {
+    let city = req.body.city;
+    let code = req.body.code;
+    let data = await getPlaceHolder(city, code);
+    if (data.cod == "404"){
+        res.render("weather", {
+            err:"The provided location does not exist"
+        });
+        return; 
+    }
+    console.log(data)
+    let name = data.name;
+    let description = data.weather[0].description;
+    let temp = data.main.temp;
+    let feels_like = data.main.feel_like;
+    let sunrise = data.sys.sunrise;
+    let sunset = data.sys.sunset;
+    res.render("weather", {
+        name, 
+        data:{description ,temp, feels_like, sunrise, sunset},
+        listExist:true
+    });    
+});
+
+app.get("/weather", (req, res) => {
+    res.render("weather");
+});
+
+
+
+
+
+
+
+
+
+
+
+
 app.get("*", (req, res) => {
     res.status(404);
-    res.render("404")
-})
+    res.render("404");
+});
+
 app.listen(3000, () => {
     console.log("Listening to port 3000");
 });
